ce637bd2b5ada89853decbee8461af539e8b552a
seeker8 <chuyo_gk87@yahoo.com.mx>
2015-11-15 21:06:55 -0600

    Remove debugger statements

--- functional_javascript/reduce.js
+++ functional_javascript/reduce.js
@@ -2,7 +2,6 @@ function countWords(inputWords) {
     // SOLUTION GOES HERE
     var res = {};
     return inputWords.reduce(function(prev, next, index, array){
-        debugger;
         prev[next] = (prev[next] || 0) + 1;
         return prev;
      }, res);
